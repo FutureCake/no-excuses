23c86105cb5d99f2fed8a3884203d72f33937048
FutureCake <thomas.c.hoek@gmail.com>
2023-03-22 14:31:50 +0100

    added popup js code to set default addictions

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,34 @@
 const addictions = document.getElementById("addictions");
 const add_addiction = document.getElementById("add-addiction");
+const new_addiction = document.getElementById("new-addiction");
 
+console.log(new_addiction);
+console.log(addictions);
+
+function create_addiction(addiction_context) {
+    const el = document.createElement('li');
+    el.textContent = addiction_context;
+    return el;
+}
+
+function set_default_addictions() {
+    set_addictions([
+        "facebook.com",
+        "instagram.com",
+        "youtube.com",
+        "youtu.be",
+        "tiktok.com"
+    ]).then((values) => {
+        values.forEach(value => {
+            addictions.insertBefore(create_addiction(value), new_addiction);
+        });
+    });
+}
+
+set_default_addictions();
+
+chrome.runtime.onInstalled.addListener((details) => {
+    if (details.reason === 'install') {
+        set_default_addictions();
+    }
+});
