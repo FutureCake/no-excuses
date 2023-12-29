362d55977bc38613d95d25052ff61056091b9a0e
FutureCake <thomas.c.hoek@gmail.com>
2023-12-30 00:43:53 +0100

    removed for now unnecessary background script

--- src/pages/Background/index.ts
+++ src/pages/Background/index.ts
@@ -1,5 +1,14 @@
-console.log('This is the background page.');
+// chrome.webRequest.onBeforeRequest.addListener((details) => {
 
-chrome.webRequest.onBeforeRequest.addListener((details) => {
-    console.log(details.url);
-}, {urls: ["<all_urls>"]});
+    // if(details.url.indexOf("www.netflix") !== -1) {
+    
+        // console.log("found netflix send message");
+
+        // chrome.runtime.sendMessage<OnUrlMatched>({
+        //     sender: "background",
+        //     message: "url matched",
+        //     url: details.url,
+        //     tabId: details.tabId
+        // });
+    // }
+// }, {urls: ["<all_urls>"]});
