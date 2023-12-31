f5472ed04428f3ac50a7a5165138bc3c64105022
FutureCake <thomas.c.hoek@gmail.com>
2023-12-31 18:51:47 +0100

    added code to make popup work with all new components. added styles

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,13 +1,13 @@
 import React, { Component, ReactNode } from 'react';
 import AddAddiction from '../../Components/AddAddiction';
-import { AddictionProps } from '../../Components/Addiction';
+import { AddictionData } from '../../Components/Addiction/Addiction';
 import Addictions from '../../Components/Addictions';
 import QuickAddAddiction from '../../Components/QuickAddAddiction';
 import { Message } from '../../Utils/types';
 import './Popup.scss';
 
 interface PopupState {
-    addictions: AddictionProps[];
+    addictions: AddictionData[];
 }
 
 class Popup extends Component<{}, PopupState> {
@@ -27,21 +27,35 @@ class Popup extends Component<{}, PopupState> {
     }
 
     async componentDidMount(): Promise<void> {
-        this.setState({
-            addictions: await chrome.storage.local.get("addictions") as AddictionProps[]
-        });
+        // this.setState({
+        //     addictions: await chrome.storage.local.get("addictions") as AddictionProps[]
+        // });
     }
 
     onExtensionMessage(msg: Message): void {
         
     }
 
-    onAddAddiction(addiction: AddictionProps): void {
-        
+    onAddAddiction(addiction: AddictionData): void {
+        this.setState((prev) => {
+            // CHECK: is it bad to modify an readonly array inplace?
+            prev.addictions.push(addiction);
+            return {
+                addictions: prev.addictions
+            }
+        });
     }
 
     onRemoveAddiction(index: number): void {
         
+        this.setState((prev) => {
+            // CHECK: is it bad to modify an readonly array inplace?
+            prev.addictions.splice(index, 1);
+            
+            return {
+                addictions: prev.addictions
+            }
+        });
     }
 
     render(): ReactNode {
@@ -49,10 +63,10 @@ class Popup extends Component<{}, PopupState> {
             <div id='popup'>
                 <h1 id='title'>Your addictions</h1>
                 <div id='addictions-manager'>
-                    <Addictions addictions={[]} onRemove={this.onRemoveAddiction}/>
-                    <AddAddiction onNew={this.onAddAddiction}/>
+                    <Addictions addictions={this.state.addictions} onRemove={this.onRemoveAddiction.bind(this)}/>
+                    <AddAddiction onNew={this.onAddAddiction.bind(this)}/>
                 </div>
-                <QuickAddAddiction onNew={this.onAddAddiction}/>
+                <QuickAddAddiction onNew={this.onAddAddiction.bind(this)}/>
             </div>
         );
     }
