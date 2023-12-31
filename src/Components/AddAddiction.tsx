012f53ca6d404a166922551876e5e17383c5142b
FutureCake <thomas.c.hoek@gmail.com>
2023-12-31 18:49:56 +0100

    added basic working functionality for add addiction component

--- src/Components/AddAddiction.tsx
+++ src/Components/AddAddiction.tsx
@@ -1,20 +1,49 @@
-import React, { Component, ReactNode } from "react";
-import { AddictionProps } from "./Addiction";
+import React, { Component, ReactNode, SyntheticEvent } from "react";
+import { AddictionData } from "./Addiction/Addiction";
 
 interface AddAddictionProps {
-    onNew: (addiction: AddictionProps) => void;
+    onNew: (addiction: AddictionData) => void;
 }
 
 class AddAddiction extends Component<AddAddictionProps> {
     
+    private newUrl?: string;
+
     constructor(props: any) {
         super(props);
+    }
+
+    createNewAddiction(): void {
+
+        // TODO: find better solution for this
+        if(!this.newUrl) return;
+        
+        this.props.onNew({
+            url: this.newUrl
+        });
+    }
+
+    onKeyPress(event: SyntheticEvent<HTMLInputElement, KeyboardEvent>): void {
+
+        this.newUrl = event.currentTarget.value;
+
+        if(event.nativeEvent.key === "Enter") {
+            this.createNewAddiction();
+        }
     }
 
     render(): ReactNode {
         return (
             <div>
-
+                <div id="add-overlay">
+                    <img id="icon" src="../assets/app/icons/add.png"/>
+                    <p id="title">Add addiction</p>
+                </div>
+                <div id="add-input">
+                    <input type="text" name="url" id="url" onKeyUp={this.onKeyPress.bind(this)}/>
+                    <img src="../assets/app/icons/confirm.png" alt="add" onClick={this.createNewAddiction.bind(this)}/>
+                    <img src="../assets/app/icons/cancel.png" alt="cancel" />
+                </div>
             </div>
         );
     }
