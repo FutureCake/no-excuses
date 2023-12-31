63d494bab2016a9704a16f05d1ffc028e4aff5bd
FutureCake <thomas.c.hoek@gmail.com>
2023-12-31 17:00:21 +0100

    add new components and helper functions to popup component

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,22 +1,60 @@
 import React, { Component, ReactNode } from 'react';
+import AddAddiction from '../../Components/AddAddiction';
+import { AddictionProps } from '../../Components/Addiction';
+import Addictions from '../../Components/Addictions';
+import QuickAddAddiction from '../../Components/QuickAddAddiction';
+import { Message } from '../../Utils/types';
 import './Popup.scss';
 
-class Popup extends Component {
-    constructor(props: any) {
+interface PopupState {
+    addictions: AddictionProps[];
+}
+
+class Popup extends Component<{}, PopupState> {
+    
+    constructor(props: {}) {
         super(props);
+        chrome.runtime.onMessage.addListener(this.onExtensionMessage);
+        
+        // REMOVE: for testing pruposes
+        this.state = {
+            addictions: [
+                {url: "wwww.youtube.com"},
+                {url: "wwww.netflix.com"},
+                {url: "wwww.amazonprime.com"},
+            ]
+        }
+    }
+
+    async componentDidMount(): Promise<void> {
+        this.setState({
+            addictions: await chrome.storage.local.get("addictions") as AddictionProps[]
+        });
+    }
+
+    onExtensionMessage(msg: Message): void {
+        
+    }
+
+    onAddAddiction(addiction: AddictionProps): void {
+        
+    }
+
+    onRemoveAddiction(index: number): void {
+        
     }
 
     render(): ReactNode {
         return (
-            <div>
-                <h1>Your addictions</h1>
-                <div>
-                    <div id='addictions'></div>
-                    <div id='new-addiction'></div>
+            <div id='popup'>
+                <h1 id='title'>Your addictions</h1>
+                <div id='addictions-manager'>
+                    <Addictions addictions={[]} onRemove={this.onRemoveAddiction}/>
+                    <AddAddiction onNew={this.onAddAddiction}/>
                 </div>
-                <div>Add current website as addiction</div>
+                <QuickAddAddiction onNew={this.onAddAddiction}/>
             </div>
-        )
+        );
     }
 }
 
