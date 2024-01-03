4e40034572cf3df10b8207575cb93c3d5cf37d83
FutureCake <thomas.c.hoek@gmail.com>
2024-01-03 19:41:43 +0100

    added a storage manager service

---
+++ src/Services/storage.service.ts
@@ -0,0 +1,49 @@
+import { BlockedDomain } from "../Components/Addiction/Addiction";
+
+async function getBlockedDomains(): Promise<BlockedDomain[]> {
+    const urlsObj = await chrome.storage.local.get("urls") as { domains: BlockedDomain[] };
+    return urlsObj.domains;
+}
+
+async function overwriteBlockedDomains(...domains: BlockedDomain[]): Promise<boolean> {
+    try {
+        await chrome.storage.local.set({ domains: domains });
+        return true;
+    } catch (err) {
+        return false;
+    }
+}
+
+async function addBlockedDomains(...newDomains: BlockedDomain[]): Promise<BlockedDomain[]> {
+    const domains = await getBlockedDomains();
+    const updated = [...domains, ...newDomains];
+    chrome.storage.local.set({
+        domains: updated
+    });
+
+    return updated;
+}
+
+async function removeBlockedDomains(...oldDomains: BlockedDomain[]): Promise<BlockedDomain[] | undefined> {
+    const domains = await getBlockedDomains();
+
+    const updated = domains.filter((domain) => {
+        return (!oldDomains.includes(domain))
+    });
+
+    const success = await overwriteBlockedDomains(...updated);
+
+    return success ? updated : undefined;
+}
+
+async function updateBlockedDomain(updated: BlockedDomain): Promise<boolean> {
+
+    const domains = await getBlockedDomains();
+    domains.map((domain) => {
+        return (domain.id === updated.id) ? updated : domain;
+    });
+    return overwriteBlockedDomains(...domains);
+}
+
+export { addBlockedDomains, getBlockedDomains, overwriteBlockedDomains, removeBlockedDomains, updateBlockedDomain };
+
