918880890434497e8573e18845e5b3bf689cf929
FutureCake <thomas.c.hoek@gmail.com>
2024-01-29 22:58:45 +0100

    updated contentmanager to load blocked domains and check current page.

--- src/pages/Content/ContentManager.tsx
+++ src/pages/Content/ContentManager.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { Root, createRoot } from "react-dom/client";
+import * as uri from "uri-js";
+import { BlockedDomain } from "../../Components/Addiction/Addiction";
+import { ExtensionMessage } from "../../Utils/types";
 
 class ContentManager {
 
     private root?: Root;
+    private blockedDomains: BlockedDomain[] = [];
 
-    constructor() {
+    public async init(): Promise<void> {
+        this.blockedDomains = await chrome.runtime.sendMessage<ExtensionMessage, BlockedDomain[]>({
+            sender: "content",
+            recipient: "background",
+            action: "get"
+        });
+
+        console.log(this.blockedDomains);
 
         if (this.checkUrlBlocked(window.location.href)) {
             this.setupOverlay();
@@ -13,7 +24,21 @@ class ContentManager {
     }
 
     private checkUrlBlocked(url: string): boolean {
-        return true;
+        const domain = uri.parse(url).host;
+        console.log(domain);
+
+        if (domain) {
+            for (let i = 0; i < this.blockedDomains.length; i++) {
+                const blocked = this.blockedDomains[i];
+                console.log("CHECK: ", domain, "===", blocked.url, " - ", blocked.url === domain);
+                if (blocked.url === domain) {
+                    console.log("MATCHEDD");
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
     private setupOverlay(): void {
