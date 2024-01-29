5435c7edc4305644600cf20dc0a60d9fa21eaa5f
FutureCake <thomas.c.hoek@gmail.com>
2024-01-29 22:58:15 +0100

    removed messy old code. added basic temp code for development progression

--- src/pages/Background/index.ts
+++ src/pages/Background/index.ts
@@ -1,42 +1,24 @@
 import { BlockedDomain } from "../../Components/Addiction/Addiction";
-import { getBlockedDomains, overwriteBlockedDomains } from "../../Services/storage.service";
-import { Message, PopupMessage, ReplyMessage } from "../../Utils/types";
+import { ExtensionMessage } from "../../Utils/types";
 
-type Reply = (res: ReplyMessage<BlockedDomain[]>) => void;
-
-chrome.runtime.onMessage.addListener(async (msg: PopupMessage | Message, _, response: Reply) => {
-    console.log("received message from: ", msg.sender);
-    if (msg.sender === "content") requestBlockedUrls(response);
-    if (msg.sender === "popup") processPopupMessage(msg as PopupMessage, response);
-});
-
-// CHECK: messy af fuck...
-async function processPopupMessage(message: PopupMessage, res: Reply) {
-    switch (message.action) {
-        case "set": setBlockedDomains(res, ...message.domains as BlockedDomain[]); break;
-        case "get": requestBlockedUrls(res); break;
+const blocked: BlockedDomain[] = [
+    {
+        id: 12334,
+        url: "www.youtube.com"
+    },
+    {
+        id: 12335,
+        url: "www.instagram.com"
+    },
+    {
+        id: 12336,
+        url: "www.buzzfeed.com"
     }
-}
+]
 
-async function setBlockedDomains(response: Reply, ...domains: BlockedDomain[]) {
-    await overwriteBlockedDomains(...domains);
-    response({
-        message: "overwrote the current blocked domains",
-        data: domains
-    });
-}
-
-async function requestBlockedUrls(response: Reply): Promise<void> {
-    const urls = await getBlockedDomains();
-    response({
-        message: "your requested blocked urls",
-        data: urls
-    });
-}
-
-
-
-
+chrome.runtime.onMessage.addListener((message: ExtensionMessage, _, response) => {
+    response(blocked);
+});
 
 
 // chrome.webRequest.onBeforeRequest.addListener((details) => {
