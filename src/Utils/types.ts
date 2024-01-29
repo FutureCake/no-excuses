4ec414d6ef9f3174926a5f6b45f0a958ee4b1481
FutureCake <thomas.c.hoek@gmail.com>
2024-01-29 22:59:28 +0100

    removed old messy message types

--- src/Utils/types.ts
+++ src/Utils/types.ts
@@ -1,27 +1,16 @@
 import { BlockedDomain } from "../Components/Addiction/Addiction";
 
-type Sender = "background" | "content" | "popup" | "newTab";
+type Entity = "background" | "content" | "popup" | "newTab";
+type Action = "update" | "remove" | "get" | "add" | "set";
 
-interface Message {
-    message: string;
-    sender: Sender;
-}
-
-interface PopupMessage extends Message {
-    action: "update" | "remove" | "get" | "add" | "set";
+interface ExtensionMessage {
+    sender: Entity;
+    recipient: Entity;
+    action?: Action;
     domains?: BlockedDomain[] | BlockedDomain;
 }
 
-
-
-interface ReplyMessage<T = void> {
-    message: string;
-    data?: T;
-}
-
 export {
-    Message,
-    PopupMessage,
-    ReplyMessage,
-    Sender
+    Action, Entity, ExtensionMessage
 };
+
