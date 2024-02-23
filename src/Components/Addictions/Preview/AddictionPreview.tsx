fe06765ba451dee45e099d8edd64b77e836d9ca3
FutureCake <thomas.c.hoek@gmail.com>
2024-02-23 12:45:15 +0100

    basic content for preview component

--- src/Components/Addictions/Preview/AddictionPreview.tsx
+++ src/Components/Addictions/Preview/AddictionPreview.tsx
@@ -2,20 +2,36 @@ import React, { Component, ReactNode } from "react";
 import { AddictionProps } from "../Addiction/Addiction";
 
 type PreviewActionType = "info" | "edit" | "delete";
+type AddictionSeverity = "severe" | "high" | "medium" | "low" | "barely" | "indeterminable";
 
 interface AddictionPreviewProps extends Pick<AddictionProps, "displayName" | "lastAccess" | "blockedOn" | "allExcuses" | "totalAccessTries"> {
     onUserAction: (type: PreviewActionType) => void;
 }
 
-class AddictionPreview extends Component<AddictionPreviewProps> {
+interface AddictionPreviewState {
+    addictionLevel: AddictionSeverity;
+    timeSinceLastAccess: string;
+    hovering: boolean;
+}
+
+class AddictionPreview extends Component<AddictionPreviewProps, AddictionPreviewState> {
     constructor(props: AddictionPreviewProps) {
         super(props);
+
+        this.state = {
+            addictionLevel: "medium",
+            timeSinceLastAccess: "5 days",
+            hovering: false
+        }
     }
 
     render(): ReactNode {
         return (
             <div className="addiction-preview">
-
+                <h2>{this.props.displayName}</h2>
+                <h3>{this.state.addictionLevel}</h3>
+                <p>{this.state.timeSinceLastAccess}</p>
+                <p>{this.props.blockedOn}</p>
             </div>
         );
     }
