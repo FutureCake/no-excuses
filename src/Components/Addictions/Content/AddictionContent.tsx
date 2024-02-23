515e93541aa33be9b61146a639d07fae1f9b1d9b
FutureCake <thomas.c.hoek@gmail.com>
2024-02-23 12:58:50 +0100

    added state for editing in content component + basic style placeholder

--- src/Components/Addictions/Content/AddictionContent.tsx
+++ src/Components/Addictions/Content/AddictionContent.tsx
@@ -2,18 +2,27 @@ import React, { Component, ReactNode } from "react";
 import { BlockedDomain } from "../Addiction/Addiction";
 
 interface AddictionContentProps extends BlockedDomain {
-    onModificationSave: (index: number) => void;
+    onModificationSave: (index: number, changes: Partial<BlockedDomain>) => void;
+    editMode: boolean;
 }
 
-class AddictionContent extends Component<AddictionContentProps> {
+interface AddictionContentState {
+    editMode: boolean;
+}
+
+class AddictionContent extends Component<AddictionContentProps, AddictionContentState> {
 
     constructor(props: AddictionContentProps) {
         super(props);
+
+        this.state = {
+            editMode: props.editMode
+        }
     }
 
     render(): ReactNode {
         return (
-            <div></div>
+            <div style={{ "height": "100px", background: "red" }}></div>
         );
     }
 }
