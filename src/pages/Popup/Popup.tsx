3aa0d328b667701a0386ab0e316be49600e7c147
FutureCake <thomas.c.hoek@gmail.com>
2024-02-23 12:45:49 +0100

    minor css size changes. rewrote app to be a FC component with context manager

--- src/pages/Popup/Popup.tsx
+++ src/pages/Popup/Popup.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import AddAddiction from '../../Components/AddAddiction/AddAddiction';
-import Addictions from '../../Components/Addictions';
 import { BlockedDomain } from '../../Components/Addictions/Addiction/Addiction';
+import AddictionsList from '../../Components/Addictions/List/AddictionsList';
 import { AddictionsContext, IAddictionsContext } from '../../Components/AddictionsContext';
 import QuickAddAddiction from '../../Components/QuickAddAddiction';
+import { ExtensionMessage } from '../../Utils/types';
 import './Popup.scss';
 
 
@@ -12,8 +13,18 @@ function Popup() {
     const [addictions, setAddictions] = useState<BlockedDomain[]>([]);
 
     useEffect(() => {
-
+        getSavedAddictions();
     });
+
+    const getSavedAddictions = useCallback(async (): Promise<void> => {
+
+        const savedAddictions = await chrome.runtime.sendMessage<ExtensionMessage, BlockedDomain[]>({
+            sender: "content",
+            recipient: "background",
+            action: "get"
+        });
+        setAddictions(savedAddictions);
+    }, [])
 
     const onAddAddiction = (addiction: BlockedDomain): void => {
         setAddictions([...addictions, addiction]);
@@ -40,24 +51,12 @@ function Popup() {
         addictions: addictions
     }
 
-    // async componentDidMount(): Promise < void> {
-    //     this.setState({
-    //         addictions: await chrome.runtime.sendMessage<ExtensionMessage, BlockedDomain[]>({
-    //             sender: "content",
-    //             recipient: "background",
-    //             action: "get"
-    //         })
-    //     });
-    // }
-
-
-
     return (
         <AddictionsContext.Provider value={ctx}>
             <div id='popup'>
                 <h1 id='title'>Your addictions</h1>
                 <div id='addictions-manager'>
-                    <Addictions addictions={addictions} onRemove={() => { }} />
+                    <AddictionsList addictions={addictions} />
                     <AddAddiction onNew={() => { }} />
                 </div>
                 <QuickAddAddiction onNew={() => { }} />
