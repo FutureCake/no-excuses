95562312cff4ad9a4045c4e9188d4fc4ffc8c80a
FutureCake <thomas.c.hoek@gmail.com>
2024-02-23 12:44:16 +0100

    updated placeholder data

--- src/pages/Background/index.ts
+++ src/pages/Background/index.ts
@@ -4,15 +4,21 @@ import { ExtensionMessage } from "../../Utils/types";
 const blocked: BlockedDomain[] = [
     {
         id: 12334,
-        domain: "www.youtube.com"
+        domain: "www.youtube.com",
+        blockedOn: "23/02/2022",
+        displayName: "Youtube"
     },
     {
         id: 12335,
-        domain: "www.instagram.com"
+        domain: "www.instagram.com",
+        blockedOn: "23/02/2022",
+        displayName: "Instagram"
     },
     {
         id: 12336,
-        domain: "www.buzzfeed.com"
+        domain: "www.buzzfeed.com",
+        blockedOn: "23/02/2022",
+        displayName: "BuzzFeed"
     }
 ]
 
