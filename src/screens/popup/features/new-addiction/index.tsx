455edd56608218371024c08779dd5d9f0fb64b43
FutureCake <thomas.c.hoek@gmail.com>
2025-08-06 19:11:10 +0200

    simplified the add addiction component

--- src/screens/popup/features/new-addiction/index.tsx
+++ src/screens/popup/features/new-addiction/index.tsx
@@ -1,39 +1,31 @@
-import React, { useState } from "react";
-import UrlActions from "./components/url-actions";
-import UrlMatches from "./components/url-matches";
-import UrlSegmentAction from "./components/url-segment-action";
-import UrlSubdomainAction from "./components/url-subdomain-action";
+import React from "react";
+import useValidUrl from "./hooks/valid-url";
 import './style.scss';
 
-export default function NewAddiction() {
+export type NewAddictionProps = {
+    onBlock: (domain: string) => void;
+}
 
-    const [url, setUrl] = useState<string | undefined>(undefined);
-    const [urlValid, setUrlValid] = useState<boolean>(false);
-    const [action, setAction] = useState<"domain" | "subdomain" | "segment" | "exact">("exact");
-    const [matches, setMatches] = useState<string[]>([]);
+export default function NewAddiction(props: NewAddictionProps) {
+
+    const { onBlock } = props;
+    const { valid, domain, setUrl, url } = useValidUrl();
 
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value;
-        setUrlValid(/^(https?:\/\/)?([a-zA-Z0-9-_.]+\.[a-zA-Z]{2,})(:\d+)?(\/[^\s]*)?(\?[^\s]*)?(#[^\s]*)?$/.test(value));
-        setUrl(e.target.value || undefined);
+        setUrl(e.target.value);
     }
 
-    const handleAction = (action: "domain" | "subdomain" | "segment" | "exact") => {
+    const addAddiction = () => {
+        if (!domain || !valid) return;
 
-        if (url === undefined) return;
-
-        setAction(action);
-        if (action === "exact") setMatches((prev) => [...prev, url]);
-        if (action === "domain") setMatches((prev) => [...prev, url.replace(/^(https?:\/\/)?([a-zA-Z0-9-_.]+\.[a-zA-Z]{2,})(:\d+)?(\/[^\s]*)?(\?[^\s]*)?(#[^\s]*)?$/, "$2")]);
+        onBlock(domain);
+        setUrl("");
     }
 
     return (
         <div id="new-addiction">
             <input id="new-addiction-input" type="text" placeholder="Add a new addiction url" value={url} onChange={onChange} />
-            {(url !== undefined && urlValid) && <UrlActions url={url} onAction={handleAction} />}
-            {(url !== undefined && action === "segment") && <UrlSegmentAction url={url} onComplete={() => { }} />}
-            {(url !== undefined && action === "subdomain") && <UrlSubdomainAction url={url} onComplete={() => { }} />}
-            {matches.length > 0 && <UrlMatches matches={matches} />}
+            {valid && <button onClick={addAddiction}>{`Block ${domain}`}</button>}
         </div>
     );
 }
