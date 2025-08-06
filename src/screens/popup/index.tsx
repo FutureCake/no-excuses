6fa4746810ec3b9abaf125f734d36094a93911d8
FutureCake <thomas.c.hoek@gmail.com>
2025-08-06 19:06:15 +0200

    fixed popup, not sure if we should use a context provider or not?

--- src/screens/popup/index.tsx
+++ src/screens/popup/index.tsx
@@ -1,22 +1,40 @@
-import React from "react";
-import AddictionsProvider from "./contexts/addictions";
+import React, { useEffect, useState } from "react";
+import { addBlockedDomains, getBlockedDomains } from "../../services/storage";
+import { isErr } from "../../utils/helpers";
+import { BlockedDomain } from "../../utils/types";
 import Addictions from "./features/addictions";
 import NewAddiction from "./features/new-addiction";
-import getAddictions from "./hooks/get-addictions";
 import './style.scss';
 
 export default function Popup() {
 
-    const addictions = getAddictions();
-    console.log("hook addictions: ", addictions);
+    const [addictions, setAddictions] = useState<BlockedDomain[]>();
+
+    useEffect(() => {
+        getBlockedDomains().then((domains) => {
+
+            if (isErr(domains)) return;
+            setAddictions(domains.value);
+        });
+    }, []);
+
+    const onBlock = async (domain: string) => {
+        const updatedDomains = await addBlockedDomains({
+            id: Math.random() * 1000,
+            url: domain,
+            name: domain
+        });
+
+        if (isErr(updatedDomains)) return;
+
+        setAddictions(updatedDomains.value);
+    }
 
     return (
-        <AddictionsProvider addictions={addictions}>
-            <div id="popup">
-                <h1 id="popup-title">Digital Addictions</h1>
-                <NewAddiction />
-                <Addictions />
-            </div>
-        </AddictionsProvider>
+        <div id="popup">
+            <h1 id="popup-title">Digital Addictions</h1>
+            <NewAddiction onBlock={onBlock} />
+            <Addictions addictions={addictions} />
+        </div>
     )
 }
