ceaa29681f5ef28925369b9ec8fdcb3edbfb3025
FutureCake <thomas.c.hoek@gmail.com>
2025-08-06 19:34:46 +0200

    much nicer

--- src/screens/popup/index.tsx
+++ src/screens/popup/index.tsx
@@ -1,39 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { addBlockedDomains, getBlockedDomains } from "../../services/storage";
-import { isErr } from "../../utils/helpers";
-import { BlockedDomain } from "../../utils/types";
+import React from "react";
+import useBlockedDomains from "../../hooks/domains";
 import Addictions from "./features/addictions";
 import NewAddiction from "./features/new-addiction";
 import './style.scss';
 
 export default function Popup() {
 
-    const [addictions, setAddictions] = useState<BlockedDomain[]>();
-
-    useEffect(() => {
-        getBlockedDomains().then((domains) => {
-
-            if (isErr(domains)) return;
-            setAddictions(domains.value);
-        });
-    }, []);
-
-    const onBlock = async (domain: string) => {
-        const updatedDomains = await addBlockedDomains({
-            id: Math.random() * 1000,
-            url: domain,
-            name: domain
-        });
-
-        if (isErr(updatedDomains)) return;
-
-        setAddictions(updatedDomains.value);
-    }
+    const addictions = useBlockedDomains();
 
     return (
         <div id="popup">
             <h1 id="popup-title">Digital Addictions</h1>
-            <NewAddiction onBlock={onBlock} />
+            <NewAddiction />
             <Addictions addictions={addictions} />
         </div>
     )
