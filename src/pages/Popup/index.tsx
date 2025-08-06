db2512e36993ef17cffac2559d4b4f50663ddb4e
FutureCake <thomas.c.hoek@gmail.com>
2025-08-06 22:16:38 +0200

    added a normalize css file to popup

--- src/pages/Popup/index.tsx
+++ src/pages/Popup/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Popup from '../../screens/popup';
+import "./normalize.css";
 
 const container = document.getElementById('app-container');
 const root = createRoot(container!);
