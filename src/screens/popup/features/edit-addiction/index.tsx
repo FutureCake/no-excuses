460f899b249293d6a215b9ab30c6bc1ada9b6cd0
FutureCake <thomas.c.hoek@gmail.com>
2025-08-10 21:30:28 +0200

    removed all the old icons and added two relevant icons

--- src/screens/popup/features/edit-addiction/index.tsx
+++ src/screens/popup/features/edit-addiction/index.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
+import objReducer from "../../../../shared/helpers/object-reducer";
+import { updateBlockedDomain } from "../../../../shared/services/storage";
+import { BlockedDomain } from "../../../../shared/types/types";
 import ContentSection from "../../components/content-section";
 import useBlockedDomain from "./hooks/blocked-domain";
+import "./style.scss";
 
 export type EditAddictionProps = {
     addictionId: number;
@@ -11,17 +15,40 @@ export default function EditAddiction(props: EditAddictionProps) {
 
     const { addictionId, onClose } = props;
     const addiction = useBlockedDomain(addictionId);
+    const [values, dispatch] = useReducer(objReducer<Omit<BlockedDomain, "url" | "id" | "failures" | "preventions">>, addiction ?? {
+        allowExcuses: false,
+        name: ""
+    });
+
+    useEffect(() => {
+        if (!addiction) return;
+        dispatch(addiction);
+    }, [addiction]);
+
+    const onSave = () => {
+        updateBlockedDomain(addictionId, (current) => {
+            return { ...current, ...values };
+        });
+    }
 
     if (!addiction) return null;
 
     return (
-        <ContentSection title={`Edit ${addiction.name}`} onClose={onClose}>
-            <label>Allow excuse</label>
-            <input type="checkbox" />
-            <div>
-                <h3>Time constraints</h3>
-            </div>
-            <button></button>
+        <ContentSection title={`Edit ${values.name}`} onClose={onClose}>
+            <section id="edit-container">
+                <div id="excuses-wrapper">
+                    <input id="excuses" name="excuses" type="checkbox" onChange={(e) => dispatch({ allowExcuses: e.target.checked })} checked={values.allowExcuses} />
+                    <label htmlFor="excuses">Allow excuses</label>
+                </div>
+                <div>
+                    <label htmlFor="name">Display name</label>
+                    <input type="text" name="name" id="name" onChange={(e) => dispatch({ name: e.target.value })} value={values.name} />
+                </div>
+                <div>
+                    <h3>Time constraints</h3>
+                </div>
+                <button onClick={onSave}>Save edits</button>
+            </section>
         </ContentSection>
     )
 }
